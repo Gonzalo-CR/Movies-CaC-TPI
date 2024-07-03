0294c7e073738f0fec5cf84cd27f1237f3c7d566
Gonzalo_CR <161965777+Gonzalo-CR@users.noreply.github.com>
2024-07-03 19:43:29 -0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,28 +2,47 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
 const { validateToken, loginPage } = require('../controllers/authController');
 const { getUserByEmail, createUser } = require('../controllers/userController');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 
 // Ruta de inicio de sesión
 router.get('/login', loginPage);
 
 // Ruta de registro de usuario
-router.post('/register', async (req, res) => {
+router.post('/register', upload.single('ProfilePicture'), async (req, res) => {
     try {
+        // Obtener datos de registro del cuerpo de la solicitud
         const { Name, Surname, Email, Password, Birthday, Countries_CountryID } = req.body;
+        const ProfilePicture = req.file ? req.file.filename : null;
 
-        if (!Name || !Surname || !Email || !Password || !Birthday || !Countries_CountryID) {
+        // Validar datos de registro
+        if (!Name || !Surname || !Email || !Password || !Birthday || !ProfilePicture || !Countries_CountryID) {
             return res.status(400).json({ error: 'Todos los campos son requeridos' });
         }
 
+        // Comprobar si el usuario ya existe
         const existingUser = await getUserByEmail(Email);
         if (existingUser) {
             return res.status(400).json({ error: 'El usuario ya existe' });
         }
 
+        // Hash la contraseña
         const hashedPassword = await bcrypt.hash(Password, 10);
-        const newUser = await createUser(Name, Surname, Email, hashedPassword, Birthday, Countries_CountryID);
+
+        // Crear nuevo usuario en la base de datos
+        const newUser = await createUser(Name, Surname, Email, hashedPassword, Birthday, ProfilePicture, Countries_CountryID);
 
         console.log('Usuario creado exitosamente!!');
         res.json({ message: 'Usuario creado exitosamente!!', userId: newUser.UserID });
@@ -36,23 +55,29 @@ router.post('/register', async (req, res) => {
 // Ruta de autenticación
 router.post('/auth', async (req, res) => {
     try {
+        // Obtener correo electrónico y contraseña del cuerpo de la solicitud
         const { Email, Password } = req.body;
 
+        // Validar correo electrónico y contraseña
         if (!Email || !Password) {
             return res.status(400).json({ error: 'Se requieren correo electrónico y contraseña' });
         }
 
+        // Consultar usuario en la base de datos por correo electrónico
         const user = await getUserByEmail(Email);
 
+        // Si el usuario no existe, enviar error
         if (!user) {
             return res.status(401).json({ error: 'Usuario no encontrado' });
         }
 
+        // Comparar la contraseña proporcionada con la contraseña hash almacenada
         const validPassword = await bcrypt.compare(Password, user.Password);
         if (!validPassword) {
             return res.status(401).json({ error: 'Contraseña incorrecta' });
         }
 
+        // Generar y enviar token de autenticación
         const token = generateToken(user.UserID);
         console.log('Inicio de sesión exitoso!!');
         res.json({ token, redirectUrl: '/' });
@@ -82,3 +107,4 @@ function generateToken(userId) {
 }
 
 module.exports = router;
+
