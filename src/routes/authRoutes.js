2e94eaa64b7483beedb0af26e2614cb7ea26c857
Gonzalo_CR <gonzalocorralesruiz@gmail.com>
2024-07-04 19:43:37 -0300

    mas ajustes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,112 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
-const { validateToken, authenticateUser, registerUser  } = require('../controllers/authController');
-const { getUserByEmail, createUser } = require('../controllers/userController');
+const { loginPage, authenticateUser, registerUser } = require('../controllers/authController');
 
+// Configuración de Multer para subir archivos
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, path.join(__dirname, '../../uploads'));
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
+        cb(null, `${Date.now()}-${file.originalname}`);
     }
 });
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 // Ruta de inicio de sesión
-router.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/html/iniciosesion.html'));
-});
+router.get('/login', loginPage);
 
 // Ruta de registro de usuario
-router.post('/register', upload.single('ProfilePicture'), async (req, res) => {
-    try {
-        // Obtener datos de registro del cuerpo de la solicitud
-        const { Name, Surname, Email, Password, Birthday, Countries_CountryID } = req.body;
-        const ProfilePicture = req.file ? req.file.filename : null;
-
-        // Validar datos de registro
-        if (!Name || !Surname || !Email || !Password || !Birthday || !ProfilePicture || !Countries_CountryID) {
-            return res.status(400).json({ error: 'Todos los campos son requeridos' });
-        }
-
-        // Comprobar si el usuario ya existe
-        const existingUser = await getUserByEmail(Email);
-        if (existingUser) {
-            return res.status(400).json({ error: 'El usuario ya existe' });
-        }
-
-        // Hash la contraseña
-        const hashedPassword = await bcrypt.hash(Password, 10);
-
-        // Crear nuevo usuario en la base de datos
-        const newUser = await createUser(Name, Surname, Email, hashedPassword, Birthday, ProfilePicture, Countries_CountryID);
-
-        console.log('Usuario creado exitosamente!!');
-        res.json({ message: 'Usuario creado exitosamente!!', userId: newUser.UserID });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
-});
+router.post('/register', upload.single('ProfilePicture'), registerUser);
 
 // Ruta de autenticación
-router.post('/auth', async (req, res) => {
-    try {
-        // Obtener correo electrónico y contraseña del cuerpo de la solicitud
-        const { Email, Password } = req.body;
-
-        // Validar correo electrónico y contraseña
-        if (!Email || !Password) {
-            return res.status(400).json({ error: 'Se requieren correo electrónico y contraseña' });
-        }
-
-        // Consultar usuario en la base de datos por correo electrónico
-        const user = await getUserByEmail(Email);
-
-        // Si el usuario no existe, enviar error
-        if (!user) {
-            return res.status(401).json({ error: 'Usuario no encontrado' });
-        }
-
-        // Comparar la contraseña proporcionada con la contraseña hash almacenada
-        const validPassword = await bcrypt.compare(Password, user.Password);
-        if (!validPassword) {
-            return res.status(401).json({ error: 'Contraseña incorrecta' });
-        }
-
-        // Generar y enviar token de autenticación
-        const token = generateToken(user.UserID);
-        console.log('Inicio de sesión exitoso!!');
-        res.json({ token, redirectUrl: '/' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
-});
-
-// Ruta protegida con API
-router.get('/api', validateToken, async (req, res) => {
-    try {
-        const user = req.user;
-        const userData = await getUserById(user.UserID);
-        res.json({ email: user.Email, userData });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
-});
-
-function generateToken(userId) {
-    const secretKey = process.env.SECRET || 'your_secret_key';
-    const payload = { userId };
-    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
-    return token;
-}
+router.post('/auth', authenticateUser);
 
 module.exports = router;
-
